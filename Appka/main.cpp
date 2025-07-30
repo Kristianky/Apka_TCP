ccf5ceabfdd46dfb0b21bf4df57ccc892e473762
Kristianky <kristiantabacek@gmail.com>
2025-07-30 15:33:21 +0200

    Okomentovanie

--- Appka/main.cpp
+++ Appka/main.cpp
@@ -4,21 +4,26 @@
 #define ID_EDIT 3
 
 wchar_t Buffer_For_Text [300];
-
+ //hwnd->okno ktoremu je sprava urcena...
+ //uint -> premmene napr z klavesnice mysi atd..
+ //wparam-> stavovy parameter doplnujuce info
+ //lparam -> dalsie parametre napr poloha kurzora atd
 LRESULT CALLBACK WindowProc(HWND hwnd,UINT umsg,WPARAM wparam,LPARAM lparam){
     switch (umsg){
+        //vytvori okna tlacidlo 1,2 a edit okienko
        case WM_CREATE:
-          CreateWindowW(L"BUTTON",L"Zobraz text",WS_VISIBLE|WS_CHILD,500,10,150,100,hwnd,(HMENU)ID_BUTTON_SHOW_TEXT,NULL,NULL);
+       //vytvori tlacidlo button->co to ma byt....zobraz text->text na tlacidle...WS ->styly a vyzor... poloha a velkost... 
+          CreateWindowW(L"BUTTON",L"Zobraz text",WS_VISIBLE|WS_CHILD,500,10,150,100,hwnd,(HMENU)ID_BUTTON_SHOW_TEXT,NULL,NULL);   
           CreateWindowW(L"BUTTON",L"Koniec",WS_VISIBLE|WS_CHILD,1000,10,150,100,hwnd,(HMENU)ID_BUTTON_END,NULL,NULL);
           CreateWindowW(L"EDIT",L"",WS_VISIBLE|WS_CHILD|WS_BORDER|ES_LEFT,10,10,150,100,hwnd,(HMENU)ID_EDIT,NULL,NULL);
         break;
         case WM_COMMAND:
-           switch (LOWORD(wparam))
+           switch (LOWORD(wparam))        //toto urcuje ktore tlacidlo bolo stlacene alebo ine stavy
            {
            case ID_BUTTON_SHOW_TEXT:
                {
-               HWND h_Edit = GetDlgItem(hwnd,ID_EDIT);
-               GetWindowTextW(hwnd,Buffer_For_Text,300);
+               HWND h_Edit = GetDlgItem(hwnd,ID_EDIT);      //urci co sa stane po stlaceni tlacidla 1 v tomto pripade precita text z edit a zapise ho do buffera
+               GetWindowTextW(h_Edit,Buffer_For_Text,300);
                MessageBoxW(hwnd,Buffer_For_Text,L"Sprava",MB_OK|MB_ICONINFORMATION);
                break;}
             case ID_BUTTON_END:
@@ -27,8 +32,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd,UINT umsg,WPARAM wparam,LPARAM lparam){
            }
            break;
            case WM_DESTROY:
-           PostQuitMessage(0);
-           break;
+           PostQuitMessage(0);              //zavrie appku
+           break; 
            default:
            return DefWindowProc(hwnd,umsg,wparam,lparam);
            break;
@@ -36,8 +41,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd,UINT umsg,WPARAM wparam,LPARAM lparam){
 return 0;
 }
 
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow){
+// histance je unikatne cislo okna
+//Histance hprevinstance je pre zastarale systemy moze byt null
+//lpwstr-> retazec priradeny pri vytvarani
+//ncmdshow-> ako sa ma hl okno zobrazovat
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow){  //main funkcia 
      const wchar_t CLASSNAME[] =  L"MojeOknoTrieda";
 
      WNDCLASSW wc {};
