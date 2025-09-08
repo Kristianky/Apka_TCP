9c2db34c435bb26b6f7bfa2f178944a54cbfe473
Kristianky <kristiantabacek@gmail.com>
2025-09-08 12:07:20 +0200

    Dokeconcenie pages

--- Apka_TCP/src/Pages/Pages.cpp
+++ Apka_TCP/src/Pages/Pages.cpp
@@ -1,12 +1,29 @@
 #include "Pages.h"
 
-Pages::Pages(){
-
+Pages::Pages()
+{
+    Windows_CLASS = new wchar_t *[6] { {L"BUTTON"},
+                                       {L"EDIT"},
+                                       {L"STATIC"},
+                                       {L"LISTBOX"},
+                                       {L"COMBOBOX"},
+                                       {L"SCROLLBAR"}};
 }
 
-Pages::~Pages(){
-    delete [] Buttons;
-    delete [] Buttons_Names;
-    delete [] Buttons_PositionsX;
-    delete [] Buttons_PositionsY;
+Pages::~Pages()
+{
+    delete[] Windows;
+    delete[] Windows_Names;
+    delete[] Windows_PositionsX;
+    delete[] Windows_PositionsY;
+    delete[] Page_Name;
+    delete[] Windows_CLASS;
+}
+
+void Pages::Create_Window(HWND hwnd)
+{
+    for (int i{}; i < Number_Of_Windows; i++)
+    {
+        CreateWindow(Windows_CLASS[i], Windows_Names[i], WS_CHILD | WS_VISIBLE, Windows_PositionsX[i], Windows_PositionsY[i], 200, 100, hwnd, (HMENU)(ID_BUTTONS + i), NULL, NULL);
+    }
 }
--- Apka_TCP/src/Pages/Pages.h
+++ Apka_TCP/src/Pages/Pages.h
@@ -6,15 +6,18 @@
 class Pages 
 {
 private:
-   HWND *Buttons;
-   wchar_t *Buttons_Names;
-   int *Buttons_PositionsX;
-   int *Buttons_PositionsY;
+   HWND *Windows;
+   int Number_Of_Windows;
+   wchar_t **Windows_CLASS;
+   wchar_t **Windows_Names;
+   int *Windows_PositionsX;
+   int *Windows_PositionsY;
+   wchar_t *Page_Name;
 
 public:
    Pages();
    virtual ~Pages();
-   virtual void Create_Window();
+   virtual void Create_Window(HWND hwnd);
 };
 
 #endif
