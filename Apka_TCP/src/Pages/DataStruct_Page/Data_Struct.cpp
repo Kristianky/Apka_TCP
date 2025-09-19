661dec71232ed64d136fd898023ad43ebd0d65ea
Kristianky <kristiantabacek@gmail.com>
2025-09-19 16:24:48 +0200

    Cmake

--- Apka_TCP/src/Pages/DataStruct_Page/Data_Struct.cpp
+++ Apka_TCP/src/Pages/DataStruct_Page/Data_Struct.cpp
@@ -1,11 +1,11 @@
 #include "Data_Struct.h"
 
 Data_Struct_Page::Data_Struct_Page(){
-    Windows_Names = new wchar_t *[5]{{L"Paint"}};
+    Windows_Names = new wchar_t *[5]{{L"Paint"},{L"ShowStauts"}};
     Windows_CLASS = new wchar_t *[5]{{L"BUTTON"}};
     Number_Of_Windows = new int{1};
-    Windows_PositionsX = new int[1]{10};
-    Windows_PositionsY = new int[1]{10};
+    Windows_PositionsX = new int[2]{10,210};
+    Windows_PositionsY = new int[2]{10,10};
     Windows = new HWND[10];
     Buttons_Bools = new bool[5];
 }
@@ -19,7 +19,7 @@ void Data_Struct_Page::Create_WindowW(HWND *Buttons,HWND Main_Hwnd,int BUTTON)
     
     for (int i{}; i < *Number_Of_Windows; i++)
     {
-         Buttons[i] = CreateWindowW(Windows_CLASS[i], Windows_Names[i], WS_CHILD | WS_VISIBLE, Windows_PositionsX[i], Windows_PositionsY[i], 200, 100, Main_Hwnd, (HMENU)(BUTTON + i), NULL, NULL);
+         Buttons[i] = CreateWindowW(Windows_CLASS[0], Windows_Names[i], WS_CHILD | WS_VISIBLE, Windows_PositionsX[i], Windows_PositionsY[i], 200, 100, Main_Hwnd, (HMENU)(BUTTON + i), NULL, NULL);
     }
    
     
@@ -30,7 +30,7 @@ void Data_Struct_Page::Cout(HDC hdc){
 }
 
 void Data_Struct_Page:: Buttons_Function (LPARAM lparam,WPARAM wparam,int &page_num,HWND *Buttons,HWND Main_hwnd,bool *Buttons_State){
-    enum BUTTONS_ID{Paint = 10000};
+    enum BUTTONS_ID{Paint = 10000,ShowWindow};
      switch(LOWORD(wparam)){
         case Paint:
              if(Buttons_State[0]){
@@ -40,5 +40,10 @@ void Data_Struct_Page:: Buttons_Function (LPARAM lparam,WPARAM wparam,int &page_
                Buttons_State[0] = true;
              }
              break;
+          case ShowWindow:
+                if(Buttons_State[0]){
+               MessageBoxW(Main_hwnd,L"True",MB_OK,MB_OK);}
+                else if(!Buttons_State[0]){
+                MessageBoxW(Main_hwnd,L"False",MB_OK,MB_OK);}
      }
 }
