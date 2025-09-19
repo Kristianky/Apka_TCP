61c7ae458437ec95113abd58fcbd31bf1c211e3d
Kristianky <kristiantabacek@gmail.com>
2025-09-19 16:17:57 +0200

    OK

--- Apka_TCP/src/Pages/DataStruct_Page/Data_Struct.cpp
+++ Apka_TCP/src/Pages/DataStruct_Page/Data_Struct.cpp
@@ -29,7 +29,7 @@ void Data_Struct_Page::Cout(HDC hdc){
      TextOutW(hdc,100,200,L"Ahoj",100);
 }
 
-void Data_Struct_Page:: Buttons_Function(LPARAM lparam,WPARAM wparam,int &page_num,HWND *Buttons,HWND Main_hwnd,bool &Buttons_State[]){
+void Data_Struct_Page:: Buttons_Function (LPARAM lparam,WPARAM wparam,int &page_num,HWND *Buttons,HWND Main_hwnd,bool *Buttons_State){
     enum BUTTONS_ID{Paint = 10000};
      switch(LOWORD(wparam)){
         case Paint:
--- Apka_TCP/src/Pages/DataStruct_Page/Data_Struct.h
+++ Apka_TCP/src/Pages/DataStruct_Page/Data_Struct.h
@@ -8,7 +8,7 @@ class Data_Struct_Page:public Pages{
          Data_Struct_Page();
          void Create_WindowW(HWND *Buttons,HWND Main_Hwnd,int BUTTON) override;
          virtual ~Data_Struct_Page();
-         virtual void Buttons_Function(LPARAM lparam,WPARAM wparam,int &page_num,HWND *Buttons,HWND Main_hwnd,bool &Buttons_State[]) override;   
+         virtual void Buttons_Function(LPARAM lparam,WPARAM wparam,int &page_num,HWND *Buttons,HWND Main_hwnd,bool *Buttons_State) override;   
          virtual void Cout(HDC hdc) override;
 };
 
