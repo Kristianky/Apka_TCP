7c25ffa36ba5d26aab6286ee2ad4c84e46af0663
Kristianky <kristiantabacek@gmail.com>
2025-11-28 11:02:46 +0100

    Zalozenie linked list class

---
+++ Apka_TCP/src/Pages/Page_All/Linked_List.h
@@ -0,0 +1,20 @@
+#ifndef _LINKED_LIST_H_
+#define _LINKED_LIST_H_
+
+struct List
+{
+    int data;
+    List *Next;
+};
+
+
+
+class LinkedList
+{
+   private:
+       List First;
+   public:
+      
+};
+
+#endif
